1dd158cd6f5fa42050d6cf781033030ea5f0d002
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-02 01:12:50 +0200

    ch100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-# Import necessary libraries
-import pandas as pd
-
-# Assuming the data is stored in a CSV file named 'text_messages.csv'
-# Load the data into a pandas DataFrame
-data = pd.read_csv('text_messages.csv')
-
-# Select relevant columns for sender, receiver, and flags
-selected_data = data[['sender', 'receiver', 'flags']]
-
-# Output the selected data
-print(selected_data)
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
