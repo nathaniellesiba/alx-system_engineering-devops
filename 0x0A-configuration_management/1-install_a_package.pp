37b4f37a518dcc7c7efcc95a9d7883f8656ba931
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 01:52:08 +0200

    T1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, install flask from pip3
 
-exec {'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem,
 }
