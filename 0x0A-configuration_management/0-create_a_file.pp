bc2afd1289981de0ba0e87e61a6e203f5d47cd42
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 01:46:16 +0200

    cpy

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-file
-{
-'/tmp/school':
-  ensure  => file,
+#creating a file in /tmp
+
+file {'/tmp/school':
   mode    => '0744',
-  owner   => 'owner_username',
-  group   => 'owner_group',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => "I love Puppet\n",
 }
 
